d558096ed6a7a8620d23d11e53d7ad11f3efff94
Jeremy Fuentes <jeremyfuentes201@gmail.com>
2025-03-03 09:34:32 -0600

    Guia III

---
+++ Temporal/Program.cs
@@ -0,0 +1,13 @@
+﻿using rectBackend.Repository;
+
+AlumnoDao alumnoDao = new AlumnoDao();
+var alumnos = alumnoDao.SelectAll();
+foreach (var item in alumnos)
+{
+    Console.WriteLine(item.Nombre);
+}
+
+Console.WriteLine("");
+
+var selectById = alumnoDao.GettById(1);
+Console.WriteLine(selectById?.Nombre);
---
+++ rectBackend/Repository/AlumnoDao.cs
@@ -0,0 +1,33 @@
+﻿using rectBackend.Context;
+using rectBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rectBackend.Repository
+{
+    public class AlumnoDao
+    {
+        #region Context
+        public RegistroAlumnoContext contexto = new RegistroAlumnoContext();
+        #endregion
+
+        #region Select All
+        public List<Alumno> SelectAll()
+        {
+            var alumno = contexto.Alumnos.ToList<Alumno>();
+            return alumno;
+        }
+        #endregion
+
+        #region Select By Id
+        public Alumno GettById(int id)
+        {
+            var alumno = contexto.Alumnos.Where(x => x.Id == id).FirstOrDefault();
+            return alumno == null ? new Alumno() : alumno;
+        }
+        #endregion
+    }
+}
